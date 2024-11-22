5f834bfebf3b03be21a53fe78b06293f904832f6
konstelasiara <frayded99@gmail.com>
2024-11-22 15:49:25 +0700

    Added checks for negative and non-numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  let finalSum = 0;
 
+  if (!args.isInteger()) return "ERROR" // check if negative
+
+  if ((!Number.isInteger(args[0])) || (!Number.isInteger[1])) {
+    return "ERROR"
+  } // check if actual integer
+
+  for(let i = 0; i < args; i++){
+    finalSum += args;
+  }
+
+  return finalSum
 };
 
 // Do not edit below this line

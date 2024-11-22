ee99d7ad70d4bc83b6ef2cbf909ae65473cb0056
konstelasiara <frayded99@gmail.com>
2024-11-22 18:02:40 +0700

    Passed tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-  
+const removeFromArray = function(arr, ...args) {
+  let removedResult = [];                  // to collect the result
+
+  for (let coll of arr) {
+    if (!args.includes(coll)) {            // if NOT part of args are in the coll
+      removedResult.push(coll);            // then push the coll array into removedResult
+    }
+  }
+
+  return removedResult
 };
 
 // Do not edit below this line

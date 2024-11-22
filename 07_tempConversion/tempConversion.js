650955ae5ca5965064a086de28e2a54dc693bf50
konstelasiara <frayded99@gmail.com>
2024-11-22 18:59:07 +0700

    Finished tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let temps = (fahrenheit - 32) * (5 / 9);
+  return Math.round(temps * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let temps = (celsius * (9/5)) + 32;
+  return Math.round(temps * 10) / 10;
 };
 
 // Do not edit below this line

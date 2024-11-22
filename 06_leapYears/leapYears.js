64889e75089c6f43903ae964e2ab28b9fa1a27cd
konstelasiara <frayded99@gmail.com>
2024-11-22 18:43:39 +0700

    Finished

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true
+  } else {
+    return false;
+  }
+  // the formula is deductible by 4 and not a century or divisible by 400
 };
 
 // Do not edit below this line
